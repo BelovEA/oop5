dcc680203f814fd83fae3de273e7b02885f57eb4
host <vrachea676@gmail.com>
2024-03-21 07:00:17 +0300

    hw5

---
+++ Main.java
@@ -0,0 +1,19 @@
+package notebook;
+
+import notebook.controller.UserController;
+import notebook.model.repository.GBRepository;
+import notebook.model.repository.impl.UserRepository;
+import notebook.util.DBConnector;
+import notebook.view.UserView;
+
+import static notebook.util.DBConnector.DB_PATH;
+import static notebook.util.DBConnector.createDB;
+
+public static void main(String[] args) {
+    createDB();
+    GBRepository fileOperation = new UserRepository(DB_PATH);
+    UserRepository userRepository = new UserRepository(fileOperation);
+    UserController controller = new UserController(userRepository);
+    UserView view = new UserView(controller);
+    view.run();
+    }
